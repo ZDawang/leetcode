02fec2a16f1ee5729ad1f9d00fb59e4dc1671486
ZDawang <zdawang@bupt.edu.cn>
2017-11-22 10:17:06 +0800

    720

--- 720_Longest_Word_in_Dictionary.py
+++ 720_Longest_Word_in_Dictionary.py
@@ -10,18 +10,33 @@
 #beats: 
 
 class Solution(object):
-    #BFS
+    #第一思路，标准BFS，只击败了百分之37
     def longestWord(self, words):
         wordSet = set(words)
-        queue = [word for word in words if len(word) == 1]
-        res = min(queue)
+        queue = [""]
+        res = ""
         while queue:
             word = queue.pop(0)
             for c in "abcdefghijklmnopqrstuvwxyz":
-                if word + c in wordSet:
-                    newword = word + c
+                newword = word + c
+                if newword in wordSet:
                     queue.append(newword)
                     res = res if len(res) > len(newword) or (len(res) == len(newword) and res < newword) else newword
         return res
 
-    
+    #在答案里看到的一种方法，找到一个比当前结果长的，再看当前字符能够从words里构成
+    #击败百分之97
+    def longestWord2(self, words):
+        wordSet = set(words)
+        res = ""
+        for word in wordSet:
+            #若当前word比res短，跳过
+            if len(word) < len(res) or (len(word) == len(res) and word < res):
+                continue
+            #查看当前word是否能从单个字符推到
+            for i in range(1, len(word) + 1):
+                if not word[:i] in wordSet:
+                    break
+            #若可以，更新res
+            res = word if i == len(word) else res
+        return res
