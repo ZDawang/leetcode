4f999762af1e0afdbe04cd0a98f628e36806a4fe
ZDawang <zdawang@bupt.edu.cn>
2018-01-07 16:09:50 +0800

    m315

--- 315_Count_of_Smaller_Numbers_After_Self.py
+++ 315_Count_of_Smaller_Numbers_After_Self.py
@@ -59,6 +59,89 @@ class Solution(object):
             bit.update(index)
         return res
 
-nums = [5, 2, 6, 1]
+
+    #线段树
+    #用下标代表范围。
+    def countSmaller2(self, nums):
+        class SegNode(object):
+            def __init__(self, l, r):
+                self.val = 0
+                self.left = None
+                self.right = None
+                self.leftRange = l
+                self.rightRange = r
+
+        class SegTree(object):
+            def __init__(self, l):
+                self.root = self.CreateTree(0, l)
+            def CreateTree(self, l, r):
+                if l > r: return None
+                #叶节点
+                if l == r: return SegNode(l, r)
+                #递归构建树
+                m = (l + r) >> 1
+                root = SegNode(l, r)
+                root.left = self.CreateTree(l, m)
+                root.right = self.CreateTree(m + 1, r)
+                return root
+
+            #这里可以不用helper，设置一个默认参数node = None就行。
+            #然后node = node or self.root.
+            def update(self, i):
+                self.updateHelper(self.root, i)
+
+            def updateHelper(self, node, i):
+                if node.leftRange == node.rightRange:
+                    node.val += 1
+                    return
+                m = (node.leftRange + node.rightRange) >> 1
+
+                if i <= m:
+                    self.updateHelper(node.left, i)
+                else:
+                    self.updateHelper(node.right, i)
+
+                node.val = node.left.val + node.right.val
+
+            def sumRange(self, i):
+                return self.sumRangeHelper(self.root, 0, i)
+
+            def sumRangeHelper(self, node, i, j):
+                if node.leftRange == i and node.rightRange == j:
+                    return node.val
+                m = (node.leftRange + node.rightRange) >> 1
+
+                if j <= m:
+                    return self.sumRangeHelper(node.left, i, j)
+                elif i > m:
+                    return self.sumRangeHelper(node.right, i, j)
+                else:
+                    return self.sumRangeHelper(node.left, i, m) + self.sumRangeHelper(node.right, m + 1, j)
+
+
+        newNums = sorted(list(set(nums)))
+        #查找数字的下标。
+        loc = {num: i for i, num in enumerate(newNums)}
+        #res
+        res = [0] * len(nums)
+        #BIT
+        segtree = SegTree(len(newNums) - 1)
+        #遍历
+        for i in range(len(nums) - 1, -1, -1):
+            index = loc[nums[i]]
+            #当不是最小数时
+            if index != 0:
+                res[i] = segtree.sumRange(index - 1)
+            #更新BIT
+            segtree.update(index)
+        return res
+
+                    
+
+
+
+
+
+nums = [5,2,6,1]
 solute = Solution()
-res = solute.countSmaller(nums)
+res = solute.countSmaller2(nums)
