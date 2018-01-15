2155c1ea34963c684c9dfde77ffefba8976b18ab
ZDawang <zdawang@bupt.edu.cn>
2018-01-15 14:54:11 +0800

    673

--- 673_Number_of_Longest_Increasing_Subsequence.py
+++ 673_Number_of_Longest_Increasing_Subsequence.py
@@ -8,8 +8,9 @@
 #time_complecity:  
 #space_complecity: 
 #beats: 
-
+from collections import defaultdict
 class Solution(object):
+    #DP
     #使用length, number存储到当前数字的最长子串长度及 最长子串个数。
     #O(n2)
     def findNumberOfLIS(self, nums):
@@ -32,23 +33,40 @@ class Solution(object):
 
     #可以做优化，像300题一样。
     #O(nlogn)
-    #dp[i]表示长度为i的递增子串的结尾最小数字。
+    #minNum[i]表示长度为i的递增子串的结尾最小数字。
     #count[i][num] 表示长度为i的递增子串的结尾为num的子串数量。
+    #size记录最长的子串长度。
     def findNumberOfLIS2(self, nums):
-        #在nums中找到比当前数字大一点的数字。
+        #在nums中找到插入的位置。
         def binarySearch(nums, num):
-            if num > nums[-1]:
+            if not nums or num > nums[-1]:
                 return len(nums)
-            if num < nums[0]:
-                return 0
             l, r = 0, len(nums) - 1
             while l < r:
                 m = (l + r) >> 1
+                if nums[m] < num:
+                    l = m + 1
+                else:
+                    r = m
+            return l
 
-
-
-
-        minNum, counts = [], []
+        if not nums: return 0
+        minNum, counts = [float("inf")] * len(nums), [defaultdict(int) for _ in range(len(nums))]
+        size = 0
         for num in nums:
+            #获取插入位置
+            index = binarySearch(minNum, num)
+            #更新minNum与size
+            minNum[index] = num
+            size = max(size, index)
+            #更新counts,将counts[index-1]中值小于num的次数的和，加入到counts[index][num]中
+            if index == 0:
+                counts[index][num] += 1
+            else:
+                counts[index][num] += sum(v for k, v in counts[index - 1].items() if k < num)
+        return sum(counts[size].values())
 
 
+nums = [1,3,5,4,7]
+solute = Solution()
+res = solute.findNumberOfLIS2(nums)
