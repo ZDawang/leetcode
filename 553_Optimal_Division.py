f3d2bff178f7dd8d6e27d33f1a921f94dcc43f06
ZDawang <zdawang@bupt.edu.cn>
2018-01-23 16:12:04 +0800

    m553

--- 553_Optimal_Division.py
+++ 553_Optimal_Division.py
@@ -10,6 +10,8 @@
 #beats: 
 
 class Solution(object):
+    #因为想要结果最大，因为分母是第一个数字，则分子要尽可能的小。
+    #因为所有的数字都大于1，所以把后面的数字挨个相除就是最小的分子。
     def optimalDivision(self, nums):
         A = map(str, nums)
         if len(A) <= 2:
